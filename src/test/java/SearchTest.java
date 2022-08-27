836957f43d78eb61ce498ba02e5b7621546e4ef0
Julia0802 <yulka_kirichok@mail.ru>
2022-08-28 00:47:21 +0400

    editting SerchTest.java: serch elements using xpath

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 public class SearchTest {
     @Test
@@ -13,10 +14,12 @@ public class SearchTest {
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.google.com/");
-       System.out.println(driver.getTitle() +" page has been opened");
-        WebElement googleAppsButton = driver.findElement(By.cssSelector("div#gbwa a"));//css locator
-        googleAppsButton.click();
-       driver.quit();
+        System.out.println(driver.getTitle() +" page has been opened");
+   //     WebElement googleAppsButton = driver.findElement(By.cssSelector("div#gbwa a"));//css locator
+   //     googleAppsButton.click();
+        List<WebElement> googleAppsButtons = driver.findElements(By.xpath("//input[@name='btnI']"));//css locator
+        googleAppsButtons.get(1).click();
+        driver.quit();
     }
 
 
